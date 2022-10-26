3f7e22487a32144f7b37b622ed4c5842b600564f
sintayehu-244 <101401891+sintayehu-244@users.noreply.github.com>
2022-10-27 01:39:12 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
+# distributes an archive to your web servers
+from fabric.api import local
 from fabric.api import env
-from fabric.api import put
 from fabric.api import run
+from fabric.api import put
+from datetime import datetime
+import os.path
 
 env.hosts = ['3.94.211.201', '34.229.70.177']
 
 
-def do_deploy(archive_path):
-    """Distributes an archive to a web server.
+def do_pack():
+    """Generates a .tgz tar gzipped archive from the contents
+        of the web_static folder of your AirBnB Clone repo
+    """
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year, dt.month,
+                                                         dt.day, dt.hour,
+                                                         dt.minute, dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
 
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+
+def do_deploy(archive_path):
+    """Distributes an archive to your web servers
     """
     if os.path.isfile(archive_path) is False:
         return False
@@ -24,26 +36,26 @@ def do_deploy(archive_path):
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
+    if run("sudo rm -rf /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
+    if run("sudo mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+    if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if run("sudo rm /tmp/{}".format(file)).failed is True:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
+    if run("sudo mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
+    if run("sudo rm -rf /data/web_static/releases/{}/web_static".
            format(name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run("sudo rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+    if run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
            format(name)).failed is True:
         return False
     return True
